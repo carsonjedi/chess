193680148d9fac9c8e88aca6d5c566c10ac9883f
Carson Miles <carsoncmiles@gmail.com>
2026-03-01 23:46:15 -0700

    Added equals and hashcode handling

--- server/src/main/java/server/Server.java
+++ server/src/main/java/server/Server.java
@@ -2,13 +2,28 @@ package server;
 
 import io.javalin.*;
 
+import java.util.Objects;
+
 public class Server {
 
     private final Javalin javalin;
 
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Server server = (Server) o;
+        return Objects.equals(javalin, server.javalin);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(javalin);
+    }
+
     public Server() {
         javalin = Javalin.create(config -> config.staticFiles.add("web"));
-
         // Register your endpoints and exception handlers here.
 
     }
